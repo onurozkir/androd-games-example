ffca4215759cba9fe5023e257b92693063445ad2
onurozkir <onurozkir@hotmail.com>
2017-09-30 00:35:21 +0300

    second commit

--- app/src/main/java/com/onurozkir/kenny/Main.java
+++ app/src/main/java/com/onurozkir/kenny/Main.java
@@ -1,13 +1,33 @@
 package com.onurozkir.kenny;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Main extends AppCompatActivity {
+
+    TextView timer;
+    TextView score;
+    ImageView column, column2, column3, column4, column5, column6, column7, column8, column9 ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void startGame(View view)
+    {
+
+    }
+
+    public void stopGame(View view)
+    {
+
+    }
+
+
 }
